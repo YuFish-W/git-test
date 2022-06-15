ea57dfac2d1cb2f17a2c6bf415bbf3314606de59
YuFish-W <1219471845@qq.com>
2022-06-15 15:12:17 +0800

    second commit

--- src/main/java/com/yufish/git.java
+++ src/main/java/com/yufish/git.java
@@ -3,5 +3,6 @@ package com.yufish;
 public class git {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("hello,world2");
     }
 }
